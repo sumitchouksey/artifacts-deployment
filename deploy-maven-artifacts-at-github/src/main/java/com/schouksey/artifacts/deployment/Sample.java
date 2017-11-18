07dc3d5e9c7557d2f7ebd2f2984cf9b87421b466
Sumit Chouksey <sumitchouksey2315@gmail.com>
2017-11-19 01:55:10 +0530

    Host Maven Artifacts at Github - Initial Commit

---
+++ deploy-maven-artifacts-at-github/src/main/java/com/schouksey/artifacts/deployment/Sample.java
@@ -0,0 +1,14 @@
+package com.schouksey.artifacts.deployment;/*
+ * Application    : maven-artifacts-deployment
+ * Package Name   : com.schouksey.artifacts.deployment
+ * Class Name     : Sample
+ * Author         : SUMIT CHOUKSEY <sumitchouksey2315@gmail.com>
+ * Created On     : 11/19/2017
+ * Description    : TODO
+ */
+
+public class Sample {
+    public static void main(String args[]){
+        System.out.println("Welcome to Maven Artifacts Deployment");
+    }
+}
